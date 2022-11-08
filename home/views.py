b13d8aa7dd991e67b0eaee9de2324e55529b0d3c
Anuj Deshmukh <anuj.deshmukh17@gmail.com>
2022-11-08 12:04:06 +0530

    flash messages on login page

--- home/views.py
+++ home/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
+from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
@@ -18,14 +19,18 @@ def loginPage(request):
         try:
             user = User.objects.get(username=username)
         except:
-            # error message
-            pass
+            messages.error(request, 'Username does not exists!')
+            return redirect('home')
+        
 
         user = authenticate(request, username=username, password=password)
 
         if user is not None:
             login(request, user)
             return redirect('home')
+        else:
+            messages.error(request, 'Invalid username or password!')
+            
 
     return render(request, 'home/login.html')
 
@@ -35,7 +40,7 @@ def logoutUser(request):
 
 @login_required(login_url='login')
 def home(request):
-    q = request.GET.get('q') if request.GET.get('q') != None else '';
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
     posts = Post.objects.filter(Q(title__icontains=q) | Q(description__icontains=q))
     return render(request, 'home/home.html', {'posts' : posts})
 
@@ -64,13 +69,13 @@ def newPost(request):
 @login_required(login_url='login')
 def updatePost(request, pid):
     if request.user.is_staff == False:
-        return HttpResponse('You cannot Update')
+        return HttpResponse('You are not authorized to edit this post')
 
     post = Post.objects.get(id=pid)
     form = PostForm(instance=post)
 
     if request.user != post.author:
-        return HttpResponse('Not your post')
+        return HttpResponse('You are not authorized to edit this post')
 
     if request.method == 'POST':
         form = PostForm(request.POST, instance=post)
@@ -88,7 +93,7 @@ def deletePost(request, pid):
     post = Post.objects.get(id=pid)
     
     if request.user != post.author:
-        return HttpResponse('Not your post')
+        return HttpResponse('You are not authorized to delete this post')
 
     if request.method == 'POST':
         post.delete()
